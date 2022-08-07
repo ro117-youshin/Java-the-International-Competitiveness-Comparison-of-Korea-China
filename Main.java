2a936e68c6497939eb97d0cc36c00e45d856160b
김유신 <86038910+ro117youshinKIM@users.noreply.github.com>
2022-08-08 01:32:47 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,10 +4,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Crawling c = new Crawling();
-		c.processing();
-//		WriteToCSV w = new WriteToCSV();
-//		w.start();
+//		Crawling c = new Crawling();
+//		c.processing();
+		WriteToCSV w = new WriteToCSV();
+		w.start();
+		Read r = new Read();
+		r.start();
 	}
 
 }
