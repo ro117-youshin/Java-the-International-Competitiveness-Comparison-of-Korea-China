0417fc81948ddd691ce66ccb2432b7c2daf68f4c
ro117youshin <livebyfaith117@naver.com>
2022-08-08 13:53:36 +0900

    메인 클래스
    리팩토링

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		Crawling c = new Crawling();
-//		c.processing();
-		WriteToCSV w = new WriteToCSV();
-		w.start();
-		Read r = new Read();
-		r.start();
+		Output output = new Output();
+		output.system();
 	}
 
 }
