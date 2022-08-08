708bf64b57a5cd555929b9298b6eb8158ab1809c
ro117youshin <livebyfaith117@naver.com>
2022-08-08 14:10:27 +0900

    중국 TSI, RCA 지수 데이터 정제하여 List에 담기

--- src/WriteChinaData.java
+++ src/WriteChinaData.java
@@ -1,11 +1,9 @@
 package competitiveness;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,13 +11,16 @@ import java.util.List;
 public class WriteChinaData {
 	
 	Calculate cal = new Calculate();
-
-	public void start() {
+	static List<String> itemData = new ArrayList<String>();
+	static List<String> TSI_China = new ArrayList<String>();
+	static List<String> RCA_China = new ArrayList<String>();
+	
+	public void processing() {
 		readCSV();
 	}
 	
 	public void readCSV() {
-		File csv = new File("C:\\Users\\liveb\\Desktop\\data02\\Data02.csv");
+		File csv = new File("C:\\Users\\YOUSHIN KIM\\Desktop\\데이터분석프로그래밍\\Data.csv");
 		BufferedReader br = null;
 		int item = 5;
 		int year = 2010;
@@ -29,12 +30,7 @@ public class WriteChinaData {
 		String[] column;
 		String TSIData;
 		String RCAData;
-		
-		List<String> itemData = new ArrayList<String>();
-		List<String> TSI_China = new ArrayList<String>();
-		List<String> RCA_China = new ArrayList<String>();
-		
-		
+
 		try {
 			br = new BufferedReader(new FileReader(csv));
 			String newLine;
@@ -45,6 +41,7 @@ public class WriteChinaData {
 			for(int index = 0; index < itemData.size(); index++) {
 				
 				if(index < 2) {
+					// add index
 					if (index == 0) {
 						HScode = new String[item];
 						HScode = itemData.get(1).split(",");
@@ -54,19 +51,20 @@ public class WriteChinaData {
 						RCA_China.add(RCAData);
 						column = new String[itemData.size()];
 					}
+					
 				} else {
+					
 					column = itemData.get(index).split(",");
-					
+					// TSI & RCA calculate
 					for(int indexOfCal = 0; indexOfCal < item; indexOfCal++) {
-						TSI_Array[indexOfCal] = cal.calculate_TSI(column[(indexOfCal * 2) + 8], column[(indexOfCal * 2) + 9]); //18
+						TSI_Array[indexOfCal] = cal.calculate_TSI(column[(indexOfCal * 2) + 8], column[(indexOfCal * 2) + 9]); 
 						RCA_Array[indexOfCal] = cal.calculate_RCA(column[(indexOfCal * 2) + 8], column[7], column[indexOfCal + 2], column[1]);
-					
 					}
-					
+					// TSI Rating
 					int[] TSI_Rating = cal.TSI_rating(TSI_Array);
 					TSIData = Integer.toString(year)  + "," + TSI_Array[0] + "," + TSI_Array[1] + "," + TSI_Array[2] + "," + TSI_Array[3] + "," + TSI_Array[4] + "," + " " + "," + TSI_Rating[0] + "," + TSI_Rating[1];
 					TSI_China.add(TSIData);
-					
+					// RCA Rating
 					int[] RCA_Rating = cal.RCA_rating(RCA_Array);
 					RCAData =  Integer.toString(year)  + "," + RCA_Array[0] + "," + RCA_Array[1] + "," + RCA_Array[2] + "," + RCA_Array[3] + "," + RCA_Array[4] + "," + " " + "," 
 					+ RCA_Rating[0] + "," + RCA_Rating[1] + "," + RCA_Rating[2] + "," + RCA_Rating[3];
@@ -83,42 +81,6 @@ public class WriteChinaData {
 			e.printStackTrace();
 		}
 		
-		for (int index = 0; index < TSI_China.size();index++) {
-			System.out.println(TSI_China.get(index));
-//			make_CSV_raw(TSI_China.get(index));
-		}
-		
-		for (int index = 0; index < TSI_China.size();index++) {
-			System.out.println(RCA_China.get(index));
-//			make_CSV_raw(TSI_China.get(index));
-		}
-		
-		
 	}
-	
-	public void make_CSV_raw(String data) { // CSV 생성
 
-		File csv = new File("C:\\Users\\liveb\\Desktop\\data02\\china.csv");
-		BufferedWriter bw = null;
-
-		try {
-			bw = new BufferedWriter(new FileWriter(csv, true));
-			bw.write(data);
-			bw.newLine();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (bw != null) {
-					bw.flush();
-					bw.close();
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-	
 }
